62933d69b320192708a0cf4ca5687cf348eb19df
Will Dutcher <william.c.dutcher@gmail.com>
2020-11-16 13:43:02 -0500

    Add JSPredict.js file.

---
+++ Pre-Bootcamp Fall 2020 FT/Dutcher_Will_JSPredict.js
@@ -0,0 +1,12 @@
+// Predict 1
+// Answer: I was born in1980
+
+// Predict 2
+// Answer: I was born in1980
+
+// Predict 3
+// Answer:
+    // Summing Numbers!
+    // num1 is: 10
+    // num2 is: 20
+    // 30
